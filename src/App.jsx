0110a84f21a0d2ce709ec88507ffac93fab472e2
DilshanGAM <malithdilshan27@gmail.com>
2025-08-04 20:38:38 +0530

    day 11 ✏️

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+  
+
+  return (
+    <>
+
+
+      <ProductCard name="Apple iPad" price="$499" image="https://www.apple.com/assets-www/en_WW/ipad/product_tile/xlarge/ipad_pro_1df5448cd_2x.png" />
+
+      <ProductCard name="Mac Book Pro" price="$1299" image="https://www.apple.com/v/macbook-pro/as/images/overview/closer-look/3d_viewer_pf_16__flhf8phqnfyq_small_2x.jpg"/>
+
+      
+    </>
+  )
+}
+
+export default App
